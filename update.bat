26aecdc03d0f7c67e1004803c8a7d3cc8a2677f2
Oksisane <ehsanmasdar@gmail.com>
2014-01-12 13:31:24 -0600

    Added and implemented getversion.vi to 4290
    Closes #6

--- update.bat
+++ update.bat
@@ -9,9 +9,17 @@
 @set PLINK_PROTOCOL=ssh
 @if not defined TERM set TERM=msys
 
-del version.txt
+del ".\LASA 4290\Robo_Others\version.txt"
+del ".\LASA 5998\Robo_Others\version.txt"
 for /f "delims=" %%x in (codename.txt) do set code=%%x
+for /f "delims=" %%i in ('git rev-list --tags --max-count=1') do set version=%%i
+for /f "delims=" %%i in ('git describe --tags "%version%" ') do set versionnumber=%%i
 for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
-echo %code% >> version.txt
-echo %branch% >> version.txt
-git rev-list HEAD | wc -l >> version.txt
+set /a numcommits=0
+for /f "delims=" %%x in ('git rev-list HEAD') do  set /a numcommits=numcommits+1
+set versionnumber=%versionnumber:~0,4%
+echo %code%Ver:%versionnumber%x >> "./LASA 4290/Robo_Others/version.txt"
+echo %code%Ver:%versionnumber%x >> "./LASA 5998/Robo_Others/version.txt"
+set final=B:%branch% C:%numcommits%
+echo %final% >> "./LASA 4290/Robo_Others/version.txt"
+echo %final% >> "./LASA 5998/Robo_Others/version.txt"
