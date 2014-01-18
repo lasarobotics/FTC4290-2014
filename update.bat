88c9bad1d97ffc714e3699818502f0462a61d88a
lasa418 <lasa418@gmail.com>
2014-01-18 10:17:28 -0600

    Fixed Version Display

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
-@del version.txt
-@for /f "delims=" %%x in (codename.txt) do set code=%%x
-@for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
-@echo %code% >> version.txt
-@echo %branch% >> version.txt
-@git rev-list HEAD | wc -l >> version.txt
+del version.txt
+for /f "delims=" %%x in (codename.txt) do set code=%%x
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+echo %code% >> version.txt
+echo %branch% >> version.txt
+git rev-list HEAD | wc -l >> version.txt
