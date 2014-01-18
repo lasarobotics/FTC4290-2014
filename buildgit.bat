2b53f998d61b8bd8a2b734506512efe74f367d26
lasa418 <lasa418@gmail.com>
2014-01-18 11:23:49 -0600

    Fixed Failed Commit

--- buildgit.bat
+++ buildgit.bat
@@ -1,7 +1,2 @@
 copy .\setup\pre-commit .\.git\hooks\
 copy .\setup\post-commit .\.git\hooks\
-git submodule update --init
-cd PortableGit
-git submodule update --init
-msys.bat
-pauses
--- update.bat
+++ update.bat
@@ -1,16 +1,3 @@
-cd PortableGit
-@rem Do not use "echo off" to not affect any child calls.
-@setlocal
-
-@rem Get the absolute path to the current directory, which is assumed to be the
-@rem Git installation root.
-@for /F "delims=" %%I in ("%~dp0") do @set git_install_root=%%~fI
-@set PATH=%git_install_root%\PortableGit\bin;%git_install_root%\PortableGit\mingw\bin;%git_install_root%\PortableGit\cmd;%PATH%
-
-@set PLINK_PROTOCOL=ssh
-@if not defined TERM set TERM=msys
-
-@cd ..
 @del version.txt
 @for /f "delims=" %%x in (codename.txt) do set code=%%x
 @for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
