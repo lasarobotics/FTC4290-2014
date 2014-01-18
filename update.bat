a5fcaa867d4f6b50acc8c8c4571f2076cee6f5f5
Oksisane <ehsanmasdar@gmail.com>
2014-01-17 18:12:48 -0600

    Remove pause from update.bat

--- update.bat
+++ update.bat
@@ -15,4 +15,3 @@ for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
 echo %code% >> version.txt
 echo %branch% >> version.txt
 git rev-list HEAD | wc -l >> version.txt
-pause
