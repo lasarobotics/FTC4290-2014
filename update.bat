160da0b35352ab168e9fc3f043d4556b489ca275
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-01-27 19:29:23 -0600

    Insert Update Semantics
    Used to ensure that every environment contains the proper buildscripts.  (Minor update)

--- update.bat
+++ update.bat
@@ -1,6 +1,11 @@
-del version.txt
-for /f "delims=" %%x in (codename.txt) do set code=%%x
-for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
-echo %code% >> version.txt
-echo %branch% >> version.txt
-git rev-list HEAD | wc -l >> version.txt
+@echo -------------------------------
+@echo Updating version information...
+@del version.txt
+@for /f "delims=" %%x in (codename.txt) do set code=%%x
+@for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+@echo %code% >> version.txt
+@echo %branch% >> version.txt
+@git rev-list HEAD | wc -l >> version.txt
+@echo Done!
+@echo -------------------------------
+@pause
